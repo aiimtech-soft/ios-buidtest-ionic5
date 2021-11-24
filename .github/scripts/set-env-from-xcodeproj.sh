ae675c8eb8c0935bbb882d3b961c046ac6dfedbf
APPLE <apple@APPLEs-MBP.domain.name>
2021-11-24 18:06:23 +0530

    seventyThird Version of App

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -4,4 +4,9 @@ set -euo pipefail
 
 SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
 PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
+PROJECT="${{ env.PROJECT_PATH }}/ios-test.xcodeproj"
+WORKSPACE="${{ env.PROJECT_PATH }}/ios-test.xcworkspace"
+OUTPUT_DIRECTORY="${{ env.PROJECT_PATH }}/outputs/"
+OUTPUT_NAME="${{ env.PRODUCT_NAME }}.ipa"
+EXPORT_METHOD="development"
 echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
